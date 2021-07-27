9603f7b986b6ca3e3a5b1451eec5f7ab63197bcc
David Garza <david.garza@huskers.unl.edu>
2021-07-26 22:20:40 -0500

    Initial skeleton setup for plugin,renamed packages

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Example
-An example greeter plugin
+# Drop Tracker
+Are you ever curious just how much loot you've dropped while questing and skilling?
+
+Well wonder no more, this plugin tracks the GE GP value of any items that you drop
+and displays it on an overlay. 
--- src/main/java/com/droptracker/DropTrackerPlugin.java
+++ src/main/java/com/droptracker/DropTrackerPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.droptracker;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Drop Tracker"
 )
-public class ExamplePlugin extends Plugin
+public class DropTrackerPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private DropTrackerConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	DropTrackerConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(DropTrackerConfig.class);
 	}
 }
